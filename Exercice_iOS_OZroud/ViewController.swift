e9af07e18dd8ac01fdb867a748826a4d6b350db6
ZROUD Oussama <ousszroud@gmail.com>
2020-10-05 15:52:37 +0200

    Initial Commit

---
+++ Exercice_iOS_OZroud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercice_iOS_OZroud
+//
+//  Created by Oussama Zroud on 10/5/20.
+//  Copyright © 2020 Oussama Zroud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
