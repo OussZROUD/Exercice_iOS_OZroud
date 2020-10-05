7c9b5a628b3dfbc920e5ccf746d35cd48d43c2cc
ZROUD Oussama <ousszroud@gmail.com>
2020-10-05 16:33:52 +0200

    feat(service) : add Constants file

---
+++ Exercice_iOS_OZroud/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Exercice_iOS_OZroud
+//
+//  Created by Oussama Zroud on 10/5/20.
+//  Copyright © 2020 Oussama Zroud. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    struct Network {
+        static let baseUrl: String = "https://raw.githubusercontent.com/leboncoin/paperclip/master/"
+        static let productEndPoint: String = "listing.json"
+        static let categoryEndPoint: String = "categories.json"
+    }
+}
