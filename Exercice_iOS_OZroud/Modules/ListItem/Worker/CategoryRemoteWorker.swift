81433c611923c0cc7756bda504085b688b2181a4
ZROUD Oussama <ousszroud@gmail.com>
2020-10-19 01:00:29 +0200

    feat (test) : add unit test for category Remote worker

--- Exercice_iOS_OZroud/Modules/ListItem/Worker/CategoryRemoteWorker.swift
+++ Exercice_iOS_OZroud/Modules/ListItem/Worker/CategoryRemoteWorker.swift
@@ -10,18 +10,38 @@ import Foundation
 
 class CategoryRemoteWorker {
     
+    var categories: [CategoryItem]?
+    var categoriesDTO: [CategoryItemDTO]?
+    var error: APIError?
+    
     func getCategoriesFromRemote(onComplete:@escaping(Result<[CategoryItemDTO], APIError>)-> Void) {
         
-        ListCategoryRequest.get.executeRequest(type: [CategoryItem].self) { (response) in
+        ListCategoryRequest.get.executeRequest(type: [CategoryItem].self) { [weak self](response) in
+            let response = self?.treatResponse(response: response)
             switch response {
             case .success(let data):
-                let dataDTO = data.map { (category) -> CategoryItemDTO in
-                    return CategoryItemDTO(categoryItem: category)
-                }
-                onComplete(.success(dataDTO))
+                onComplete(.success(data))
             case .failure(let error):
                 onComplete(.failure(error))
+            case .none:
+                onComplete(.failure(.dataFailed))
             }
+        }
+    }
+    
+    func treatResponse(response: Result<[CategoryItem], APIError>) -> Result<[CategoryItemDTO], APIError>{
+        switch response {
+        case .success(let data):
+            categories = data
+            let dataDTO = data.map { (category) -> CategoryItemDTO in
+                return CategoryItemDTO(categoryItem: category)
+            }
+            self.categoriesDTO = dataDTO
+            return .success(dataDTO)
+            
+        case .failure(let error):
+            self.error = error
+            return .failure(error)
         }
     }
 }
---
+++ Exercice_iOS_OZroudTests/Workers/CategoryRemoteWorkerTests.swift
@@ -0,0 +1,83 @@
+//
+//  CategoryRemoteWorkerTests.swift
+//  Exercice_iOS_OZroudTests
+//
+//  Created by Oussama Zroud on 10/16/20.
+//  Copyright © 2020 Oussama Zroud. All rights reserved.
+//
+
+import XCTest
+@testable import Exercice_iOS_OZroud
+
+class CategoryRemoteWorkerTests: XCTestCase {
+    
+    var worker: CategoryRemoteWorker!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        worker = CategoryRemoteWorker()
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_success_response() throws {
+        let result = worker.treatResponse(response: MokeAPIResponse.categorySuccessResult)
+        switch result {
+        case .success(let data):
+            XCTAssertEqual(data.count, 3)
+        case .failure(_):
+            XCTFail()
+        }
+    }
+    
+    func test_failure_response_errorResultDataFailed() throws {
+        let result = worker.treatResponse(response: MokeAPIResponse.categoryErrorResultDataFailed)
+        switch result {
+        case .success(_):
+            XCTFail("should be failure insted of success")
+        case .failure(let error):
+            XCTAssertEqual(APIError.dataFailed.message,error.message)
+        }
+    }
+    
+    func test_failure_response_errorResultInvalidURL() throws {
+        let result = worker.treatResponse(response: MokeAPIResponse.categoryErrorResultInvalidURL)
+        switch result {
+        case .success(_):
+            XCTFail("should be failure insted of success")
+        case .failure(let error):
+            XCTAssertEqual(APIError.invalidURL(url: "testURL").message,error.message)
+        }
+    }
+    
+    func test_failure_response_errorResultParsingError() throws {
+        let result = worker.treatResponse(response: MokeAPIResponse.categoryErrorResultParsingError)
+        switch result {
+        case .success(_):
+            XCTFail("should be failure insted of success")
+        case .failure(let error):
+            XCTAssertEqual(APIError.parsingError.message,error.message)
+        }
+    }
+    
+    func test_failure_wrong_error() throws {
+        let result = worker.treatResponse(response: MokeAPIResponse.categoryErrorResultParsingError)
+        switch result {
+        case .success(_):
+            XCTFail("should be failure insted of success")
+        case .failure(let error):
+            XCTAssertNotEqual(APIError.dataFailed.message, error.message)
+            XCTAssertNotEqual(APIError.invalidURL(url: "").message, error.message)
+            XCTAssertEqual(APIError.parsingError.message, error.message)
+        }
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
